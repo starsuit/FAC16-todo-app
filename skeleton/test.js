5c5f02866eef24738a1701b690e707cc129a324a
Samantha Dickeson <samlucydickeson@gmail.com>
2019-03-13 14:36:56 +0000

    add first test

--- skeleton/test.js
+++ skeleton/test.js
@@ -1,7 +1,14 @@
-var test = require('tape');
-var logic = require('./logic');
+var test = require("tape");
+var logic = require("./logic");
 
-test('Example test', function(t) {
+test("Example test", function(t) {
   t.pass();
+  t.end();
+});
+
+test("addTodo returns an object", t => {
+  const actual = typeof logic.addTodo([], "newTodo");
+  const expected = "object";
+  t.equal(actual, expected, "addTodo should return an object");
   t.end();
 });
