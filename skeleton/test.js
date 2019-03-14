43865ed01bebee33113e0143870162966496b7a1
Samantha Dickeson <samlucydickeson@gmail.com>
2019-03-14 10:26:06 +0000

    update arry test

--- skeleton/test.js
+++ skeleton/test.js
@@ -6,10 +6,21 @@ test("Example test", function(t) {
   t.end();
 });
 
-test("addTodo returns an object", t => {
-  const actual = typeof logic.addTodo([], "newTodo");
-  const expected = "object";
-  t.equal(actual, expected, "addTodo should return an object");
+test("addTodo returns an object with description, id and done", t => {
+  const actual = logic.addTodo([], "tester");
+  const expected = [
+    {
+      id: 1,
+      description: "tester",
+      done: false
+    }
+  ];
+  t.equal(Array.isArray(actual), true, "addTodo should return an object");
+  t.deepEqual(
+    actual,
+    expected,
+    "addTodo should return an object with description, id and done"
+  );
   t.end();
 });
 
@@ -28,45 +39,48 @@ test("addTodo returns an object", t => {
 //   t.end();
 // });
 
-test("addTodo returns an object with description, id and done", t => {
-  const actual = logic.addTodo([], "tester");
-  const expected = [
-    {
-      id: 2,
-      description: "tester",
-      done: false
-    }
-  ];
-  t.deepEqual(
-    actual,
-    expected,
-    "addTodo should return an object with description, id and done"
-  );
-  t.end();
-});
+// test("addTodo returns an object with description, id and done", t => {
+//   const actual = logic.addTodo([], "tester");
+//   const expected = [
+//     {
+//       id: 2,
+//       description: "tester",
+//       done: false
+//     }
+//   ];
+//   t.deepEqual(
+//     actual,
+//     expected,
+//     "addTodo should return an object with description, id and done"
+//   );
+//   t.end();
+// });
 
 //test for adding object onto end of (object) array
 
 test("addTodo add object to existing array(of objects)", t => {
-  const actual = logic.addTodo([
-    {
-    id: 0,
-    description: "tester",
-    done: false
-    }
-], "do some code");
+  const actual = logic.addTodo(
+    [
+      {
+        id: 0,
+        description: "tester",
+        done: false
+      }
+    ],
+    "do some code"
+  );
   const expected = [
     {
-    id: 0,
-    description: "tester",
-    done: false
+      id: 0,
+      description: "tester",
+      done: false
     },
     {
-    id: 3,
-    description: "do some code",
-    done: false
+      id: 2,
+      description: "do some code",
+      done: false
     }
-  ]
+  ];
   t.deepEqual(actual, expected, "addTodo should add object to existing array");
   t.end();
 });
